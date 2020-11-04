fc9a13b75f45fd746690e6f41fe6d7663c4ece69
ksimaster <ksimasterlord@gmail.com>
2020-11-04 21:53:36 +0300

    Добавлены передвижения игрока без анимаций передвижения
    Добавлены основные передвижения игрока с поворотом
    Создана точка респауна для игрока
    Сделан тригер коллайдер для смерти от падения
    Созданы три начальные скрипта для взаимодействия

---
+++ Izolyat/Assets/Scripts/DiePlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiePlayerScript : MonoBehaviour
+{
+    public GameObject respawn;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = respawn.transform.position;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Izolyat/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 20f;
+    public int jumpForce = 16000;
+    private Rigidbody2D rb;
+
+    private bool faceRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right*moveX*speed*Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector2.up * jumpForce);
+
+        if (moveX > 0 && !faceRight)
+            flip();
+        else if (moveX < 0 && faceRight)
+            flip();
+
+
+
+    }
+
+    void flip()
+    {
+        faceRight = !faceRight;
+        transform.localScale = new Vector3(transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
+    }
+}
